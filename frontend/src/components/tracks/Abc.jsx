8bd11ab9c0b7cd557fcb2f190eebf15f3cec9693
himanshiarora14 <himanshiaroraxx@gmail.com>
2025-06-13 10:54:45 +0530

    Technical Workshop Done

---
+++ frontend/src/components/tracks/Abc.jsx
@@ -0,0 +1,208 @@
+import React from "react";
+
+function Abc(){
+    return(
+    <div>
+        <div className="flex flex-col items-center justify-center p-4">
+            
+            <div class="bg-[#040833] text-white w-full max-w-[1000px] w-auto mx-auto mt-10 p-6 sm:p-8 md:p-10 rounded-lg shadow-lg text-left" id="sec-1">
+  <div className='flex- center w-full max-w-[1000px]'>
+  <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Embedded system design</h2>
+
+  <ul class="list-disc list-inside space-y-4 text-sm sm:text-base md:text-lg">
+    <li>
+      Detailed study of any one microcontroller
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>8051</li>
+        <li>AVR (ATMEGA 16/32)</li>
+        <li>ARM7 (LPC2138)</li>
+        <li>ARM CORTEX (STM32)</li>
+      </ul>
+    </li>
+
+    <li>
+      Embedded C programming
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Data Types</li>
+        <li>Arrays, Functions & Pointers</li>
+        <li>Use of Keil and other compilers</li>
+      </ul>
+    </li>
+
+    <li>
+      Sensors and Actuators
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Sensors and Actuators</li>
+        <li>LED, LCD, Motor etc.</li>
+      </ul>
+    </li>
+
+    <li>
+      On chip interfaces
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Timer, PWM, RTC etc.</li>
+        <li>ADC & DAC</li>
+      </ul>
+    </li>
+
+    <li>
+      On chip communication protocols
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>USART, SPI, I2C</li>
+      </ul>
+    </li>
+
+    <li>Research based case studies & projects</li>
+  </ul>
+  </div>
+</div>
+<div class="bg-[#040833] text-white w-full max-w-[1000px] mx-auto mt-10 p-6 sm:p-8 md:p-10 rounded-lg shadow-lg text-left" id="sec-2">
+  <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Internet of things</h2>
+
+  <ul class="list-disc list-inside space-y-4 text-sm sm:text-base md:text-lg">
+
+    <li>
+      Python Basics for IOT
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Data Types</li>
+        <li>Arrays & Functions</li>
+        <li>File Handling</li>
+      </ul>
+    </li>
+
+    <li>
+      Study of IOT boards
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Arduino</li>
+        <li>NodeMcu</li>
+        <li>Raspberry Pi</li>
+      </ul>
+    </li>
+
+    <li>
+      Sensors and Actuators
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Switch, DHT11, MQ2 etc.</li>
+        <li>LED, LCD, Motor etc.</li>
+      </ul>
+    </li>
+
+    <li>
+      Data handling with cloud
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>With IOT Devices</li>
+        <li>With Web Interface</li>
+      </ul>
+    </li>
+
+    <li>
+      Networking Protocols for IOT and Devices
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>6LOWPAN, BLE etc.</li>
+        <li>RFID, NFC etc.</li>
+      </ul>
+    </li>
+
+    <li>Research based case studies & projects</li>
+  </ul>
+</div>
+<div class="bg-[#040833] text-white w-full max-w-[1000px] mx-auto mt-10 p-6 sm:p-8 md:p-10 rounded-lg shadow-lg text-left" id="sec-3">
+  <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Digital System Design using <span>VHDL</span></h2>
+
+  <ul class="list-disc list-inside space-y-4 text-sm sm:text-base md:text-lg">
+    <li>
+      VHDL language constructs
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Conditional statements</li>
+        <li>Various loops & Data types</li>
+        <li>Functions & Packages</li>
+      </ul>
+    </li>
+
+    <li>
+      Circuit modelling (designing) techniques
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Dataflow</li>
+        <li>Behavioural</li>
+        <li>Concurrent (Mixed)</li>
+      </ul>
+    </li>
+
+    <li>
+      Designing of combinational circuits
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Gates, Adders, Subtractor etc.</li>
+        <li>Encoders, decoders, MUX, Demux</li>
+      </ul>
+    </li>
+
+    <li>
+      Designing of sequential circuits
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Flip flops, Registers, Counters</li>
+        <li>Memories, State Machines etc.</li>
+      </ul>
+    </li>
+
+    <li>
+      Xilinx/Vivado Tool
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Area, Timing & Power Analysis</li>
+        <li>FPGA Implementation</li>
+      </ul>
+    </li>
+
+    <li>Research based case studies & projects</li>
+  </ul>
+</div>
+<div class="bg-[#040833] text-white w-full max-w-[1000px] mx-auto mt-10 p-6 sm:p-8 md:p-10 rounded-lg shadow-lg text-left" id="sec-4">
+  <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Python + Machine Learning</h2>
+
+  <ul class="list-disc list-inside space-y-4 text-sm sm:text-base md:text-lg">
+
+    <li>
+      Python Basics
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Data Types & Operators</li>
+        <li>Arrays (List, Tuple, Sets, Dictionary)</li>
+        <li>Functions & File Handling</li>
+        <li>Concept of Oops</li>
+      </ul>
+    </li>
+
+    <li>
+      Python Packages
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Numpy for Matrices</li>
+        <li>PIL for Image Processing</li>
+        <li>Pandas for Data Handling</li>
+        <li>Scikit for Machine Learning</li>
+        <li>Matplotlib for Data Plotting</li>
+      </ul>
+    </li>
+
+    <li>
+      Machine Learning
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Supervised & Unsupervised Learning</li>
+        <li>Python Framework (Anaconda)</li>
+      </ul>
+    </li>
+
+    <li>
+      ML Algorithms with mathematical concepts
+      <ul class="list-[circle] ml-6 mt-2 space-y-1">
+        <li>Regression, Naïve Bayes, KNN, SVM</li>
+        <li>Decision Tree, ANN etc.</li>
+      </ul>
+    </li>
+
+    <li>Research based case studies & projects</li>
+  </ul>
+</div>
+        </div>
+    </div>
+    );
+}
+
+export default Abc
--- frontend/src/pages/TechnicalWorkshop.jsx
+++ frontend/src/pages/TechnicalWorkshop.jsx
@@ -1,8 +1,134 @@
 import React from 'react'
-
+// import courseDetails from './components/tracks/courseDetails.jsx';
+import Abc from '../components/tracks/Abc';
 function TechnicalWorkshop() {
+
+  const handleClickAndScroll = () => {
+    const targetElement = document.getElementById('sec-1');
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+  const handleClickAndScroll2 = () => {
+    const targetElement = document.getElementById('sec-2');
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  const handleClickAndScroll3 = () => {
+    const targetElement = document.getElementById('sec-3');
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  const handleClickAndScroll4 = () => {
+    const targetElement = document.getElementById('sec-4');
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
   return (
-    <div>TechnicalWorkshop</div>
+    <div className="text-center mt-8 text-6xl font-bold text-[#1d3b58]">WCISE-RESEARCH ORIENTED WORKSHOP
+
+      <h1 className="text-xl sm:text-2xl md:text-3xl text-center mt-4 sm:mt-6 text-[#1d3b58] px-4 sm:px-6">
+  Under the Aegis of Ministry of Micro, Small & Medium Enterprises (MSME), Government of India
+</h1>
+
+      <div
+  className="bg-cover bg-center bg-no-repeat mt-10 p-6 sm:p-10 text-xl font-bold text-[#1d3b58] text-justify"
+  style={{ backgroundImage: "url('./src/images/Untitled design (1).png')" }}
+>
+  <p className="p-4 rounded">
+    The technology is playing an important role in formulating a good research article or project.
+    The in-depth knowledge of technology provides the researchers to convert their ideas into latest innovations.
+    During our conferences and social meetings, we got feedback from our participants that there is a lack of information on the net, books and research articles about the application of latest technology from research perspective.
+    This motivates WCISE research group to collaborate with the experienced people in industry and academia to fill this gap.
+    This led to the start of WCISE-RESEARCH ORIENTED WORKSHOP on latest technologies as per the need of market.
+    Initially, we are starting with four technologies as per availability of domain specific people.
+    The support and requirement of audience will provide us the feedback to include more courses or not.
+  </p>
+</div>
+      <div className="bg-[#040833] text-white font-bold text-xl px-10 py-4 rounded-full inline-block ml-8 mt-10">
+        COURSES OFFERED
+      </div>
+      <div className="flex justify-center px-4 mt-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+
+          <div onClick={handleClickAndScroll} className=" md:shadow-md hover:shadow-lg hover:shadow-blue-900 cursor-pointer transition-all w-[400px] h-[300px] bg-white rounded-2xl shadow-lg border-10 border-[#0a0e3f] flex items-center justify-center">
+            <img src=".\src\images\embedded systems.png" alt="Embedded Systems"
+              className="object-contain w-full h-full rounded-xl p-4" />
+          </div>
+
+          <div onClick={handleClickAndScroll2} className=" md:shadow-md hover:shadow-lg hover:shadow-blue-900 cursor-pointer transition-all w-[400px] h-[300px] bg-white rounded-2xl shadow-lg border-10 border-[#0a0e3f] flex items-center justify-center">
+            <img src=".\src\images\IOT.png" alt="Internet of Things"
+              className="object-contain w-full h-full rounded-xl p-4" />
+          </div>
+
+          <div onClick={handleClickAndScroll4} className=" md:shadow-md hover:shadow-lg hover:shadow-blue-900 cursor-pointer transition-all w-[400px] h-[300px] bg-white rounded-2xl shadow-lg border-10 border-[#0a0e3f] flex items-center justify-center">
+            <img src=".\src\images\python and ml.png" alt="Python + ML"
+              className="object-contain w-full h-full rounded-xl p-4" />
+          </div>
+
+          <div onClick={handleClickAndScroll3} className=" md:shadow-md hover:shadow-lg hover:shadow-blue-900 cursor-pointer transition-all w-[400px] h-[300px] bg-white rounded-2xl shadow-lg border-10 border-[#0a0e3f] flex items-center justify-center">
+            <img src='.\src\images\digital system design.png' alt='digital system design'
+              className='object-contain w-full h-full rounded-xl p-4' />
+          </div>
+
+        </div>
+        
+      </div>
+      <Abc/>
+      <p className="text-xl text-justify text center mt-10 font-bold text-[#1d3b58] mx-15">For detailed course content Email at manoj.md.et@gmail.com or whatsup at +919667006019.</p>
+      <div className="bg-[#0a0e3f] text-white font-semibold text-base sm:text-lg px-6 sm:px-12 py-3 rounded-full inline-block mt-7">
+    REWARDS
+  </div>
+
+        <div className="mx-4 sm:mx-10 text-base sm:text-lg md:text-2xl">
+  <ul className="mt-3 text-justify font-bold list-disc ml-4 sm:ml-6 space-y-2">
+    <li>Globally recognized certificate from government approved MSME enterprise.</li>
+    <li>Workshop will be conducted on 30th December 2022 at conference venue.</li>
+    <li>Workshop will be conducted by industry experts/academicians/researchers as per their domain expertise.</li>
+    <li>All the courses are designed with research oriented exposure to bridge the gap between academia and industry.</li>
+  </ul>
+  <br />
+</div>
+
+        <div className="bg-[#0a0e3f] text-white font-semibold text-base sm:text-lg px-6 sm:px-12 py-3 rounded-full inline-block mt-7">
+  REGISTRATION PROCEDURE
+</div>
+
+<div className="mx-4 sm:mx-10 text-base sm:text-lg md:text-2xl">
+  <ul className="mt-3 text-justify font-bold list-disc ml-4 sm:ml-6 space-y-2">
+    <li>Email your choice at manoj.md.et@gmail.com or WhatsApp at +91 9667006019.</li>
+    <li>Submit the registration fees at <a href="https://wcise.co.in/register" className="underline text-blue-300">wcise.co.in/register</a></li>
+    <li>Fill the Google Form at <a href="https://forms.gle/m4JidARm6WEaWBMM6" className="underline text-blue-300">forms.gle/m4JidARm6WEaWBMM6</a></li>
+    <li>For more information WhatsApp at +91 9667006019</li>
+  </ul>
+  <br />
+</div>
+
+      {/* <courseDetails/> */}
+    </div>
+
+
   )
 }
 
