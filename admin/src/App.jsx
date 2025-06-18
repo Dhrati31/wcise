87326d69d7d4f955cc53ddd273168a3068f59fc2
ARYANSH Chauhan <146801011+aryansh2616@users.noreply.github.com>
2025-06-18 13:25:17 +0530

    temp

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,17 @@
 import AnimatedSidebar from "./components/Navbar/AnimatedSidebar.jsx";
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return(
+
+    <>
+    <Routes>
+        <Route path='/' element = { <AnimatedSidebar/>} />
+    </Routes>
+    </>
    
-    <AnimatedSidebar/>
+   
   
   );
 }
