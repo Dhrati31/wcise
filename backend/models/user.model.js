33497970bf6dccccd3f77c1903264919cb7bddf1
himanshiarora14 <himanshiaroraxx@gmail.com>
2025-07-04 15:10:46 +0530

    Some issues in MongoDb

---
+++ backend/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Author', 'Editor', 'Reviewer'], required: true }
+});
+
+const userModel = mongoose.model.User || mongoose.model('User', userSchema);
+
+module.exports = userModel;
