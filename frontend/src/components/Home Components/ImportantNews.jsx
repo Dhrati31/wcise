db4de585322663e2e139fd35d1553837d624ac44
Dhrati31 <dhratikaushik05@gmail.com>
2025-11-05 14:29:31 +0530

    space

--- frontend/src/components/Home Components/ImportantNews.jsx
+++ frontend/src/components/Home Components/ImportantNews.jsx
@@ -18,9 +18,9 @@ function ImportantNews() {
             }}
             className="absolute w-full text-center"
           >
-            <p>20th December 2025 will be conference tour to Kohlarn Island.</p> <br /> <br /> <br /> <br />
-            <p>25th December 2025 will be conference tour to Kohlarn Island.</p> <br /> <br /> <br /> <br />
-            <p>29th December 2025 will be conference tour to Kohlarn Island.</p> <br /> <br /> <br /> <br />
+            <p>20th December 2025 will be conference tour to Kohlarn Island.</p> <br /> 
+            <p>25th December 2025 will be conference tour to Kohlarn Island.</p>  <br />  
+            <p>29th December 2025 will be conference tour to Kohlarn Island.</p> <br /> 
           </motion.div>
         </div>
       </div>
